f74ebf35cc44055cbf8ac1210cde7d68720ddb16
zamderax <zamderax@gmail.com>
2025-08-06 10:30:22 -0700

    Remove unused global actor PlatformActor from Platform.swift

--- Sources/TransportServices/Platforms/Platform.swift
+++ Sources/TransportServices/Platforms/Platform.swift
@@ -13,11 +13,6 @@ import Foundation
 #endif
 #endif
 
-@globalActor 
-actor PlatformActor: GlobalActor {
-    static let shared = PlatformActor()
-}
-
 /// Platform-specific implementation of the Transport Services API
 public protocol Platform: Sendable {
     /// Create a connection object for this platform
